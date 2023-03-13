59465f3b93c6af0dff11f474414e425992daaaa2
jereycui2008 <jeremycuid@gmail.com>
2023-03-13 16:57:14 -0700

    I did calculator sleepy head test score

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,15 +12,20 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String nickels = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
-
+int Manynickels = Integer.parseInt(nickels);
+double nickelamount = Manynickels * 0.05;
 		// Ask the user how many dimes they have, and convert their answer
-
+String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+int Manydimes = Integer.parseInt(dimes);
+double dimeamount = Manydimes * 0.10;
 		// Ask the user how many quarters they have, and convert their answer
-
+String quarters = JOptionPane.showInputDialog("How many quarters do you have?");
+int Manyquarters = Integer.parseInt(quarters);
+double quarteramount = Manyquarters * 0.25;
 		// Calculate how much money the user has.  Hint: Use a double variable 
-
+JOptionPane.showMessageDialog(null, "you have $" + (nickelamount + dimeamount + quarteramount));
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
 
 	}
---
+++ src/_01_double/_2_test_scores/test_scores.java
@@ -0,0 +1,35 @@
+package _01_double._2_test_scores;
+
+import javax.swing.JOptionPane;
+
+public class test_scores {
+
+	
+	public static void main(String[] args) {
+	
+	String score = JOptionPane.showInputDialog("What score did you get on your test?");
+	double testscore = Double.parseDouble(score);
+	if (testscore > 85) {
+		JOptionPane.showMessageDialog(null, "Wow, thats amazing! You must have studied really hard for that test!");
+	}
+	if (testscore < 85 && testscore > 70) {
+		JOptionPane.showMessageDialog(null, "That's a good score! good job!");
+	}
+	if (testscore < 70 && testscore > 50) {
+		JOptionPane.showMessageDialog(null, "It's alright");
+	}
+	if (testscore < 50 && testscore > 45) {
+		JOptionPane.showMessageDialog(null, "You tried your best!");
+	}
+	if (testscore < 45 && testscore > 35) {
+		JOptionPane.showMessageDialog(null, "Did you study?");
+	}
+	if (testscore < 35) {
+		JOptionPane.showMessageDialog(null, "Did you even try?");
+	}
+	
+		
+		
+	}	
+	
+}
