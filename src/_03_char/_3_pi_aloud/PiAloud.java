adbbd7ce9b1ca37f00131cd6a83a1b54a523f000
jereycui2008 <jeremycuid@gmail.com>
2023-05-08 16:55:28 -0700

    I did pi aloud

--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -2,6 +2,8 @@ package _03_char._3_pi_aloud;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 import game_tools.Sound;
 
 public class PiAloud {
@@ -16,7 +18,8 @@ public class PiAloud {
     // 1. Make a main method and make sure your program can run
     public static void main(String[] args) {
     // 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
-  String pi = "3.14159265358979323846";
+  String pi;
+  pi = "3.14159265358979323846";
     // 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
   System.out.println(pi.charAt(0));
  System.out.println(pi.charAt(1)); 
@@ -24,20 +27,35 @@ public class PiAloud {
     // Check the console shows 3.1
 
     // 4. Print ALL the digits of the Pi String, putting each digit on a new line  (hint: use a loop)
-for(int i=0; i<0; i++) {
-	
+for( int i=0; i < pi.length(); i++ ) {
+	System.out.println(pi.charAt(i));
 }
     // 5. Skip this step if your computer cannot play sounds.
-    //if (canPlaySounds) {
+    //if (canPlaySounds) 
         // Use the Sound.speak() method to speak each digit of Pi.
-        Sound.speak(pi);
-    }
+Sound.speak(pi);
+    
 
 
     // [CHALLENGE]
     // *6. Get a character from the user using the getInputFromUser() method
+ getInputFromUser();
     // *7. Compare the users' char to the next digit of Pi
+ 
+ String number = JOptionPane.showInputDialog("Choose the next number for pi");
+ char numberChar = number.charAt(0);
+		if (numberChar==pi.charAt(3)) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "wrong");
+		}
+
+	 
+ 
     // *8. If they are correct, print out "correct". If they are not, print "incorrect"
+    
+    }
 
 
     /********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
